603b859ac98c1c887480f3890a81697315c6b74a
Jeremiah Schugt <jeremiah.r.schugt@gmail.com>
2021-03-01 15:36:39 -0600

    add image forlder and alt description

--- scripts/card.js
+++ scripts/card.js
@@ -1,7 +1,7 @@
 export const cardData = (data) => {
 
     return (`<h2>${data.name}</h2>
-            <img src="${data.image}">
+            <img src="${data.image}" alt="${data.alt}">
             <a href="${data.link}">Learn More</a>`);
 
 }
