5bf91bf7e25c3093b430a62bb26d51745d9dc3a0
Jeremiah Schugt <jeremiah.r.schugt@gmail.com>
2021-03-01 15:22:40 -0600

    Set up file structure and initial functions

---
+++ samples/cities.js
@@ -0,0 +1,11 @@
+const cities = [
+    
+    {name: "city1", image:"picutre.jpeg",link:"linkme"},
+    {name: "city2", image:"picutre.jpeg",link:"linkme"},
+    {name: "city3", image:"picutre.jpeg",link:"linkme"}
+];
+
+
+export const cityData = () => {
+    return cities;
+}
---
+++ scripts/card.js
@@ -0,0 +1,8 @@
+export const cardData = (data) => {
+
+    return (`<h2>${data.name}</h2>
+            <img src="${data.image}">
+            <a href="${data.link}">Learn More</a>`);
+
+}
+
