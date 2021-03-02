2fb50064ae0a6a496c793f6854a15357a8111515
Chadwell-Clark <wcclark23@comcast.net>
2021-03-02 10:58:35 -0600

    basic malaysia functionality, added html, main.js to malaysia

--- scripts/malaysia/malaysia.js
+++ scripts/malaysia/malaysia.js
@@ -1,22 +1,13 @@
 import { cardData } from "../card.js";
 
 
-// malaysiaData(cityData, citydata);
-// malaysiaData(landmarkData, landmarkdata);
-// malaysiaData(peopleData, peopledata);
-
-import { cityData } from "cities/citiesData.js";
-import { landmarkData } from "landmark/landmarkData.js";
-import { peopleData } from "people/peopleData.js";
-
 export const malaysiaData = (dataList, tagId) => {
     const DOMLocation = document.querySelector(tagId);
     let HTMLRep = "<div>";
-    for (const data in dataList) {
+    for (const data of dataList) {
         HTMLRep += cardData(data);
     }
     HTMLRep += "</div>";
 
     DOMLocation.innerHTML += HTMLRep;
-
 }
