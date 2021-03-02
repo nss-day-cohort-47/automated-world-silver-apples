88fc02afd2b39490aaf5b1afd028736a76c9ec13
Jeremiah Schugt <jeremiah.r.schugt@gmail.com>
2021-03-02 15:29:56 -0600

    Fix errors round with HTML

--- scripts/germany/landmarks/landmarks.js
+++ scripts/germany/landmarks/landmarks.js
@@ -1,8 +1,8 @@
 const landmarks = [
 
-    { name: "Schloss Neuschwanstein", image: "./images/germany/schlossN.jpg", link: "https://en.wikipedia.org/wiki/Neuschwanstein_Castle" },
-    { name: "Sanssouci Park", image: "./images/germany/Sanssouci-Park-and-Palace.jpg", link: "https://en.wikipedia.org/wiki/Sanssouci_Park" },
-    { name: "Brandenburg Gate", image: "./images/germany/brandenburggate.jpg", link: "https://en.wikipedia.org/wiki/Brandenburg_Gate" }
+    { name: "Schloss Neuschwanstein", image: "./images/germany/schlossN.jpg",alt: "Schloss Neushwanstein", link: "https://en.wikipedia.org/wiki/Neuschwanstein_Castle" },
+    { name: "Sanssouci Park", image: "./images/germany/Sanssouci-Park-and-Palace.jpg", alt: "Sanssouci Park",link: "https://en.wikipedia.org/wiki/Sanssouci_Park" },
+    { name: "Brandenburg Gate", image: "./images/germany/brandenburggate.jpg",alt: "Brandenburg Gate", link: "https://en.wikipedia.org/wiki/Brandenburg_Gate" }
 ];
 
 
--- scripts/germany/people/people.js
+++ scripts/germany/people/people.js
@@ -2,9 +2,9 @@
 
 const people = [
 
-    { name: "Albert Einstein", image: "./images/germany/albert-einstein-7.jpg", link: "https://en.wikipedia.org/wiki/Albert_Einstein" },
-    { name: "Johanes Gutenberg", image: "./images/germany/Gutenberg.jpg", link: "https://en.wikipedia.org/wiki/Johannes_Gutenberg" },
-    { name: "Martin Luther", image: "./images/germany/martinluther.jpg", link: "https://en.wikipedia.org/wiki/Martin_Luther" }
+    { name: "Albert Einstein", image: "./images/germany/albert-einstein-7.jpg",alt:"Albert Einstein Photo", link: "https://en.wikipedia.org/wiki/Albert_Einstein" },
+    { name: "Johanes Gutenberg", image: "./images/germany/Gutenberg.jpg",alt:"Johannes Gutenburg Photo", link: "https://en.wikipedia.org/wiki/Johannes_Gutenberg" },
+    { name: "Martin Luther", image: "./images/germany/martinluther.jpg",alt:"Martin Luther Photo", link: "https://en.wikipedia.org/wiki/Martin_Luther" }
 ];
 
 
