6adf6c6e7b73a304c176df6f9afd7e19b50f6a49
Jeremiah Schugt <jeremiah.r.schugt@gmail.com>
2021-03-02 10:56:31 -0600

    update main.css

--- scripts/germany/cities/cities.js
+++ scripts/germany/cities/cities.js
@@ -2,7 +2,7 @@ const cities = [
 
     { name: "Berlin", image: "./images/germany/berlin.jpg", alt: "Photo of berlin", link: "https://en.wikipedia.org/wiki/Berlin" },
     { name: "Frankfurt", image: "./images/germany/Frankfurt.jpg", alt: "photo of frankfurt", link: "https://en.wikipedia.org/wiki/Frankfurt" },
-    { name: "Leipzig", image: "./images/germany/leipzip.jpg", alt: "leipzig", link: "https://en.wikipedia.org/wiki/Leipzig" }
+    { name: "Leipzig", image: "./images/germany/leipzig.jpg", alt: "leipzig", link: "https://en.wikipedia.org/wiki/Leipzig" }
 ];
 
 
