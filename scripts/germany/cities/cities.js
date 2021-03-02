c294b295ab0478670a788d422d06a4678f2cea0a
Jeremiah Schugt <jeremiah.r.schugt@gmail.com>
2021-03-02 07:36:16 -0600

    Added german images and data

--- scripts/germany/cities/cities.js
+++ scripts/germany/cities/cities.js
@@ -1,8 +1,8 @@
 const cities = [
     
-    {name: "Berlin", image:"picutre.jpeg",link:"linkme"},
-    {name: "Frankfurt", image:"picutre.jpeg",link:"linkme"},
-    {name: "Hamburg", image:"picutre.jpeg",link:"linkme"}
+    {name: "Berlin", image:"../../../images/germany/berlin.jpg",link:"https://en.wikipedia.org/wiki/Berlin"},
+    {name: "Frankfurt", image:"../../images/germany/Frankfurt.jpg",link:"https://en.wikipedia.org/wiki/Frankfurt"},
+    {name: "Leipzig", image:"../../../images/germany/leipsiz.jpg",link:"https://en.wikipedia.org/wiki/Leipzig"}
 ];
 
 
